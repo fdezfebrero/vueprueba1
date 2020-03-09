e2a5a101d40cc0e34c8e530ef2056bae54c52fd0
unknown <javiferfeb@live.com>
2020-03-09 22:58:05 +0100

    añadimos a github

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <h1>Lista de Participantes</h1>
+
+      <v-spacer></v-spacer>
+
+
+    </v-app-bar>
+
+    <v-content>
+      <Listapp/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Listapp from './components/Listapp';
+/*import participants from './participants.json';*/
+
+export default {
+  name: 'App',
+
+  components: {
+    Listapp,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/Listapp.vue
@@ -0,0 +1,90 @@
+<template>
+<v-card>
+  <v-card-title>
+      
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Busqueda por Email"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+  <v-data-table
+    :headers="headers"
+    :items="participants"
+    :items-per-page="10"
+    :search="search"
+    class="elevation-1"  
+  >
+  </v-data-table>
+  </v-card>
+
+</template>
+
+
+
+<script>
+import participants from '../participants.json';
+
+  export default {
+    name: 'Listapp',
+    props: {
+      firstname: {
+        type: String,
+        required: true,
+      },
+      lastname: {
+        type: String,
+        required: true,
+      },
+      email: {
+        type: String,
+        required: true,
+      },
+      created: {
+        type: Number,
+        required: true,
+      },
+      paymentoption: {
+        type: String,
+        required: true,
+      },
+      state: {
+        type: String,
+        required: true,
+        default: 'waitinglist',
+      }
+    
+  },
+
+    data () {
+      return {
+        search: '',
+        headers: [
+          {
+            text: 'Firstname',
+            align: 'start',
+            /*sortable: false,*/
+            value: 'firstname',
+            filterable: false,
+          },
+          { text: 'Lastname', value: 'lastname', filterable: false, },
+          { text: 'Email', value: 'email' },
+          { text: 'Created', value: 'created', filterable: false, },
+          { text: 'paymentoption', value: 'paymentoption', filterable: false,},
+          { text: 'State', value: 'state', filterable: false, },
+        ],
+        participants: []
+    }
+    },
+
+    mounted() {
+      this.participants=participants;
+    },
+
+  }
+ 
+  
+</script>
